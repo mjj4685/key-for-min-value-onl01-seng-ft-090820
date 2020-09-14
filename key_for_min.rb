30d99b3b6e2fe517403d7f1a4b3a766e97f716ab
MJ Johnson <mjohnson4685@gmail.com>
2020-09-14 16:11:12 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,12 +3,16 @@
 
 def key_for_min_value(name_hash)
   num = 0
-   keynum = nil
+   lowkey = nil
 
    name_hash.each do |num,value|
-     if num == 0 || value < lowest_value
+     if num == 0 || value < num
          num = value
-         keynum = key
+         lowkey = key
        end
-
+   end
+lowkey
 end
+
+ikea = {:chair => 25, :table => 85, :mattress => 450}
+key_for_min_value(ikea)
